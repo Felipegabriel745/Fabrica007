818c11681b159d37808eb2848c06c27f6514e21c
arthurhenrique02 <felipekessialive820@gmail.com>
2023-08-28 17:17:29 -0300

    dev

---
+++ atividade01.py
@@ -0,0 +1,9 @@
+# Crie um programa e diga um nuero que mostre o sucessor e o antecessor
+numero = int(input("Digie seu numero: "))
+
+recebaSucesso = numero + 1
+recebaAntecessor = numero -1
+
+
+print(f"Esse é o sucessor do seu numero: {recebaSucesso}")
+print(f"Esse é o antecessor do seu numero: {recebaAntecessor}")
